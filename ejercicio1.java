93bcf2be4e362043435a5d0999f2589014d5dbfd
SCIS-PC09 <jose quintero>
2026-09-22 07:07:21 -0600

    entrga parcial

--- ejercicio1.java
+++
@@ -1,5 +0,0 @@
-package parcial01-0192758;
-
-public class ejercicio1 {
-    
-}
---
+++ lass.java
@@ -0,0 +1,25 @@
+package parcial01-0192758;
+
+public class lass ejercicio1 {
+    
+}Scaner Scaner = new.Scaner(System./**
+ * ejercicio1
+ */
+public interface ejercicio1 {
+
+    int.arryas()= new.int(10)
+
+    int logitud = arryas.lengeth
+
+for (int i = 0; i < longitud; i++)
+
+    system.out.println( " ingrese la cantidad de paquetes"+(i+1))
+
+    consumo(i)=Scaner.nexInt();
+
+    if consumo(i) = Scaner.nexInt();
+    
+
+
+
+})
