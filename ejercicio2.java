9b1dd4794059e53eaac5edbc7a2b71509194a7fe
SCIS-PC09 <jose quintero>
2026-09-22 07:05:16 -0600

    entrega parcial

---
+++ ejercicio2.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+public class ejercicio2 {
+    public static void main(String[] args) {
+        int matriz()()= new int(4)(5);
+
+        int longitud=matriz.lengeth;
+        for (int i = 0; j < matriz (i).lengeth; j++) 
+            System.out.printl( "ingrse las cantidades vendidas");
+        matriz(i)(j)= Scanner.nexInt();
+
+        for (int i =0; i < longitud; i++ )
+            for (int j=0; )
+    }
+}
